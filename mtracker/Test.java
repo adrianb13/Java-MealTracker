b1af30ac143cf21782a42c41e214c35710e53365
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-12-03 18:17:15 -0800

    updates

--- mtracker/Test.java
+++ mtracker/Test.java
@@ -2,23 +2,30 @@ package mtracker;
 
 class Test {
   public static void main(String[] args) {
-    User user = new User("Adrian", "Briones", "adrianb13@yahoo.com", "35", 80);
+    //User info for User class
+    User user = new User("New", "User", "email@yahoo.com", "30", 80);
     user.getUserInfo();
 
+    //Attaches User to MealTracker class
     MealTracker meals = new MealTracker(user);
+
+    //Meal to be entered in the MealTracker
     Meal meal = new Meal("Lunch");
 
     System.out.println(String.format("%s is the new meal to be entered.", meal.getName().toUpperCase()));
     System.out.println();
 
+    //Categories from Category class = predetermined for testing
     Category category = Category.Protein;
     Category category2 = Category.Fruit;
     Category category3 = Category.Vegetable;
 
+    //Food items created in the Food class
     Food food = new Food("Meat", 11.8, 0.0, 26.1, category);
-    Food food2 = new Food("Orange", 0.2, 16.8, 1.7, category2);
-    Food food3 = new Food("Spinach", 0.1, 0.1, 0.9, category3);
+    Food food2 = new Food("Orange", 0.2, 23.6, 1.8, category2);
+    Food food3 = new Food("Spinach", 0.4, 3.6, 2.9, category3);
 
+    //Meat Details
     System.out.println(food.getName());
     System.out.println(food.getCalories());
     System.out.println(food.getFat());
@@ -27,6 +34,7 @@ class Test {
     System.out.println(food.getCategory());
     System.out.println();
 
+    //Orange Details
     System.out.println(food2.getName());
     System.out.println(food2.getCalories());
     System.out.println(food2.getFat());
@@ -35,6 +43,7 @@ class Test {
     System.out.println(food2.getCategory());
     System.out.println();
 
+    //Spinach Details
     System.out.println(food3.getName());
     System.out.println(food3.getCalories());
     System.out.println(food3.getFat());
@@ -43,7 +52,7 @@ class Test {
     System.out.println(food3.getCategory());
     System.out.println();
 
-    
+    //Food items added to current meal of "Lunch"
     meal.addFood(food);
     meal.addFood(food2);
     meal.addFood(food3);
@@ -51,6 +60,7 @@ class Test {
     meal.viewFoods(); // List of food items
     System.out.println();
 
+    //Meal details for all food consumed during meal
     System.out.printf("Total calories are %s. \n", meal.getCalories());
     System.out.printf("Total fat is %s. \n", meal.getFat());
     System.out.printf("Total carbs are %s. \n",meal.getCarbohydrates());
@@ -58,9 +68,11 @@ class Test {
     System.out.printf("Was this a complete meal?: %s \n",meal.isComplete());
     System.out.println();
 
+    //Adds meal to the MealTracker
     meals.addMeal(meal);
     System.out.println();
 
+    //Shows User's List of Meals
     System.out.printf("%s's ", user.getFirstName());
     meals.viewMeals();
   }  
